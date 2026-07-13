5555605c4998508f47cfd38dea1051adfa29b492
LuoJuly <luojuly517@qq.com>
2026-07-13 19:42:49 +0800

    Update lineage_xpeng.mk

--- lineage_xpeng.mk
+++ lineage_xpeng.mk
@@ -26,36 +26,3 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="xpeng_g-user 12 S1RXS32.50-13-25 5fb68-c44485 release-keys" \
     BuildFingerprint=motorola/xpeng_g/xpeng:12/S1RXS32.50-13-25/5fb68-c44485:user/release-keys \
     DeviceProduct=xpeng_retail
-
-# AVIUM_VERSION_APPEND_TIME_OF_DAY is a boolean flag to indicate
-# whether to append time of day to the build date.
-AVIUM_VERSION_APPEND_TIME_OF_DAY := false
-# Maintainer
-# AVIUM_MAINTAINER is a string that represents the maintainer of the build.
-AVIUM_MAINTAINER := LuoJuly
-
-# Settings
-# Soc model name
-AVIUM_SETTINGS_SOC_MODEL_NAME := Snapdragon 888 Plus
-# Device code name
-AVIUM_SETTINGS_DEVICE_CODENAME := Moto Edge S30
-
-# GMS
-# WITH_GMS is a boolean flag to indicate 
-# whether to include Google Mobile Services (GMS) in the build.
-WITH_GMS := false
-# LatinIMEGooglePrebuilt
-# Only works on vanilla builds,
-# GMS builds will use the Google IME from GMS.
-TARGET_INCLUDE_GOOGLEIME := true
-TARGET_GOOGLEIME_OVERRIDE_IME := true
-
-# Spoof Props
-# Set to true to enable spoofing fake props.
-# For letting apps think they are running on a locked device.
-AVIUM_FORCE_SET_FAKE_PROP := true
-
-# Blur Effect
-# The blur usually enabled on Android 16 QPR2.
-# If the blur not enabled, set to true to force enable blur for SystemUI.
-TARGET_FORCE_ENABLE_BLUR := true
